9b1d40dc5612f53b39b03d410a2329edb0e106b0
lilgionate <140750280+lilgionate@users.noreply.github.com>
2024-03-15 19:41:29 -0700

    server.js update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { ApolloServer } = require('@apollo/server-express'); // Change import statement
-const { expressMiddleware } = require('@apollo/server-express');
+const { ApolloServer } = require('@apollo/server'); // Change import statement
+const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
